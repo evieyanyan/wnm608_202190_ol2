eba5fdb264693b5a9534f99ffa824d4ee9bc07dc
evieyanyan <125115882+evieyanyan@users.noreply.github.com>
2023-05-29 21:26:21 -0700

    Add files via upload

--- yan.wang/parts/header.php
+++ yan.wang/parts/header.php
@@ -8,14 +8,16 @@
 	<header class="navbar">
 		<div class="container display-flex">
 			<div class="flex-none">
-				<h1 style="font-family: --font-theme-title:june-expt-variable, sans-serif; color: var(--color-white);">Black Sand Beach Gift Shop</h1>
+				<a href="index.php">
+				<img src="img/logo.png" style="height: 40px;margin-top: 12px;height: 100px;" alt=""></a>
+				<!-- <h1 style="font-family: --font-theme-title:june-expt-variable, sans-serif; color: var(--color-white);">Black Sand Beach Gift Shop</h1> -->
 			</div>
 			<div class="flex-stretch"></div>
 		<nav class="flex-none nav">
-			<ul class="container display-flex">
+			<ul class="container display-flex" style="margin-top: 40px;">
 				<li><a href="index.php">Home</a></li>
 				<li><a href="products.php">Products</a></li>
-				<li><a href="login.php">login</a></li>
+				<!-- <li><a href="login.php">login</a></li> -->
 				<li><a href="product_cart.php">
 					<span>Cart</span>
 					<span class="badge"><?= makeCartBadge();?></span>
